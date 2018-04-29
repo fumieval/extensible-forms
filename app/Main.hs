a36445d4d0c9fe4300975ffb26ec03eb40493fd0
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-04-29 14:49:18 +0900

    request parsing

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,17 @@
 
 module Main where
 
+import Network.HTTP.Types
+import Network.Wai
+import Network.Wai.Parse
+import Network.Wai.Handler.Warp
 import Web.Forms.Extensible
 import Web.Forms.Extensible.Bulma
 import Control.Lens
 import Data.Extensible
 import Data.Text (Text)
-import Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import qualified Data.Text as T
+import qualified Text.Blaze.Html.Renderer.Utf8 as Utf8
 import Text.Hamlet
 
 type TestFields =
@@ -21,7 +26,10 @@ testForm = defaultForm
   & #gender . formInput
     .~ Select ["Select gender", "Male", "Female", "Other"]
 
-main = putStrLn $ renderHtml [shamlet|<!DOCTYPE html>
+main = runEnv 8080 $ \req sendResp -> do
+  (params, _) <- parseRequestBodyEx defaultParseRequestBodyOptions lbsBackEnd req
+  let result = T.pack $ show $ htraverse (fmap (Field . Identity) . getField) $ parseForm testForm params
+  sendResp $ responseBuilder status200 [] $ Utf8.renderHtmlBuilder [shamlet|<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
@@ -31,11 +39,14 @@ main = putStrLn $ renderHtml [shamlet|<!DOCTYPE html>
     <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js">
   <body>
   <section class="section">
+    <div>
+      #{result}
     <div class="container">
       <h1 class="title">
         Hello World
       <p class="subtitle">
         My first website with <strong>Bulma</strong>!
-      #{form}|]
-  where
-    form = buildForm buildBulma testForm
+      <form action="/" method="post">
+        #{buildForm buildBulma testForm}
+        <div class="control">
+          <button class="button is-primary">Submit</button>|]
