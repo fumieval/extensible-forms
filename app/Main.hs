7332a36ab2d131b7469266d6995fb0a7f77357b9
Fumiaki Kinoshita <fumiexcel@gmail.com>
2018-04-29 13:00:07 +0900

    fields as a GADT

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
-{-# LANGUAGE OverloadedLabels, TypeFamilies #-}
+{-# LANGUAGE OverloadedLabels, TypeFamilies, QuasiQuotes #-}
 
 module Main where
 
 import Web.Forms.Extensible
+import Web.Forms.Extensible.Bulma
+import Control.Lens
 import Data.Extensible
 import Data.Text (Text)
 import Text.Blaze.Html.Renderer.Pretty (renderHtml)
+import Text.Hamlet
 
 type TestFields =
   [ "firstName" >: Text
@@ -14,9 +17,25 @@ type TestFields =
   ]
 
 testForm :: Form TestFields
-testForm = #firstName @= text "First Name"
-  <: #lastName @= text "Last Name"
-  <: #gender @= select ["Male", "Female", "Other"]
-  <: nil
+testForm = defaultForm
+  & #gender . formInput
+    .~ Select ["Select gender", "Male", "Female", "Other"]
 
-main = putStrLn $ renderHtml $ simpleHtml testForm
+main = putStrLn $ renderHtml [shamlet|<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hello Bulma!</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.0/css/bulma.min.css">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js">
+  <body>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      #{form}|]
+  where
+    form = buildForm buildBulma testForm
--- src/Web/Forms/Extensible.hs
+++ src/Web/Forms/Extensible.hs
@@ -1,43 +1,6 @@
-{-# LANGUAGE TypeFamilies #-}
-module Web.Forms.Extensible where
+{-# LANGUAGE TypeFamilies, DeriveFunctor #-}
+module Web.Forms.Extensible
+  ( module Web.Forms.Extensible.Field )
+  where
 
-import qualified Text.Blaze.Html as B
-import qualified Text.Blaze.Html5 as B
-import qualified Data.ByteString as B
-import Data.Extensible
-import Data.Monoid
-import Data.Text (Text)
-import qualified Data.Text.Encoding as T
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as H
-import Data.Optional
-import GHC.TypeLits
-
-type Form = RecordOf Input
-
-text :: Text
-  -> Input Text
-text ph = Input
-  { inputToHtml = H.input
-    H.! H.type_ "text"
-    H.! H.placeholder (H.textValue ph)
-  , parseValue = pure . T.decodeUtf8
-  }
-
-select :: [Text] -> Input Text
-select xs = Input
-  { inputToHtml = H.select $ mapM_ (H.option . H.text) xs
-  , parseValue = pure . T.decodeUtf8
-  }
-
-data Input a = Input
-  { inputToHtml :: H.Html
-  , parseValue :: B.ByteString -> Either String a
-  }
-
-instance Wrapper Input where
-  type Repr Input a = Input a
-  _Wrapper = id
-
-simpleHtml :: Forall (KeyIs KnownSymbol) xs => Form xs -> H.Html
-simpleHtml = hfoldMap (inputToHtml . getField)
+import Web.Forms.Extensible.Field
